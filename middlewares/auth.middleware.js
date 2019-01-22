25d105b3601ee7030fa4ca165bc4713385616134
Shyam-V <42064993+Shyam-V@users.noreply.github.com>
2019-01-23 01:16:01 +0530

    Add files via upload

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,37 @@
+const logger = require('../utils/logger')
+
+exports.is_Authenticated = (req, res, next) => {
+    if (req.user) {
+        return next()
+    } else {
+        res.render('/login')
+    }
+}
+
+exports.is_admin=(req,res,next) =>{
+    if(req.user.user_type==2) {
+        return next()
+    } else {
+        logger.error('User is unauthorized')
+        res.status(403).send({"message": "User is unauthorized."})
+    }
+}
+
+exports.is_eb=(req,res,next)=> {
+    if(req.user.user_type==1) {
+        return next()
+    } else {
+        logger.error('User is unauthorized')
+        res.status(403).send({"message": "User is unauthorized."})
+    }
+}
+
+exports.is_delegate=(req,res,next)=> {
+    if(req.user.user_type==0) {
+        return next()
+    } else {
+        logger.error('User is unauthorized')
+        res.status(403).send({"message": "User is unauthorized."})
+    }
+}
+
