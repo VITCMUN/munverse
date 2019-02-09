255f0fb50e33e4caae9496edb1fe08bd71f1b0de
Shantanu sINGH <shaneyasingh13@gmail.com>
2019-02-10 00:16:02 +0530

    updated test

--- test/api/auth/auth.test.js
+++ test/api/auth/auth.test.js
@@ -56,9 +56,7 @@ it('signup failure', (done)=>{
     .post('/signup')
     .send({ password: "1234", username: post['username'] })
     .end((err, res) => {
-      expect((res)=>{
-        return res==200 || res==304
-      })
+      expect(res.status).to.be.oneOf([200,304])
       done()
     })
 })
