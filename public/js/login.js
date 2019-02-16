d2dead56cf38ae980878d0fc9b5f255325711f93
Amey Parundekar <parundekaramey@gmail.com>
2019-02-16 20:46:59 +0530

    Integrated auth

--- public/js/login.js
+++ public/js/login.js
@@ -1,29 +1,25 @@
-window.onload = () => {
-    
-    function login() {
-        var payload = {
-            "username": document.getElementById('username').value,
-            "password": document.getElementById('password').value
-        }
-        var xhttp = new XMLHttpRequest()
-        xhttp.onreadystatechange = () => {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    window.location.href = '/'
-                } else if ([302, 304].indexOf(this.status) > -1) {
-                    window.location.href = this.redirectUrl
-                } else {
-                    document.getElementById('error_message').innerHTML = this.responseText
-                }
-            } 
-        }
-        xhttp.open('POST', '/login', true)
-        xhttp.setRequestHeader('Content-type', 'application/json')
-        xhttp.send(JSON.stringify(payload))
-    }
+$(document).ready(function () {
+    $("#login_button").on("click", function () {
+        var username = $("#username").val()  //Change the ids here if you choose 
+        var password = $("#password").val()  //to change the views later.
+        login(username, password) //Logs in the user.
+    })
+})
 
-    document.getElementById('login_button').onclick = login
+function login(username, password) {
+    loginreq = new XMLHttpRequest();
+
+    loginreq.onload = function () {
+        if (loginreq.readystate = XMLHttpRequest.DONE) {
+            if (loginreq.status === 200 || loginreq.status === 304) {
+                window.location.href = '/'
+            }else{
+                $("#error_message").html(loginreq.responseText)
+            }
+        }
+    }
+    loginreq.open('POST', 'http://localhost:8080/login', true);
+    loginreq.setRequestHeader('Content-Type', 'application/json');
+    loginreq.send(JSON.stringify({ username: username, password: password }));
 
 }
-
-  
