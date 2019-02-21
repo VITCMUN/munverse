da14b8fc6f9e107acab2ff4a3b5099be0bc971f9
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-02-22 01:20:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const config = require('./config')
 const express = require('express')
+const socket = require("socket.io")
+const ejs = require("ejs")
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const passport = require('passport')
@@ -10,48 +12,48 @@ const logger = require('./utils/logger')
 const auth_route = require('./routes/auth.route')
 const app = express()
 
+// set up
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.static('media'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-if (process.env.NODE_ENV === 'test') {
-    db_url = "mongodb://localhost:27017/test"
-} else {
+app.use(bodyParser.urlencoded({extended: false}))
+let db_url_prod = "mongodb://localhost:27017/munverse"
+let db_url_test="mongodb://localhost:27017/test"
+var db_url=""
+if (process.env.NODE_ENV !== 'test') {
     app.use(morgan('dev'))
-    db_url = "mongodb://localhost:27017/munverse"
+    db_url=db_url_prod
+} else if (process.env.NODE_ENV !== 'production') {
+  db_url=db_url_test
 }
-
 app.use(require('express-session')({
     secret: 'Silicon Valley',
     resave: false,
     saveUninitialized: false
 }))
-
 app.use(passport.initialize())
 app.use(passport.session())
-
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 app.use(auth_route)
 
-mongoose.connect(db_url, { useNewUrlParser: true })
+mongoose.connect(db_url, {useNewUrlParser: true})
 mongoose.Promise = global.Promise
-
 let db = mongoose.connection
 db.on('error', console.error.bind(console, 'Error connecting to MongoDB'))
-
 app.get('/server-status', (req, res) => {
     res.status(200).send('Server is up!')
 })
 
 // start the server
-app.listen(config.port, '0.0.0.0', () => {
+const server = app.listen(config.port, '0.0.0.0', ()=>{
     logger.info(`munverse started on ${config.port}`)
 })
+
+var io = require('socket.io')(server)
+require('./routes/message.route')(app,io)
 
 // expose to the test suite
 module.exports = app
