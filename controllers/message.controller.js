89333aa9245d34e1545b9855d2c672ee6821fca4
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-02-22 01:05:20 +0530

    Add files via upload

---
+++ controllers/message.controller.js
@@ -0,0 +1,40 @@
+var _  = require('lodash')
+
+// required 
+exports.getchatroom = (req,res) => {
+	// global variable
+    user_name = user.username
+	res.render("../public/main.html")
+}
+
+// load users 
+var users = {} // User -> id pairs
+
+
+exports.connected = (socket) =>{
+    socket.emit("allusers",{
+        users: users
+    })
+    users[user_name] = socket.id
+    socket.broadcast.emit("userconnected",{
+        id : socket.id,
+        name : user_name
+    })
+    //console.log(users)    
+}
+
+exports.disconnected = () =>{
+    delete users[user_name]
+	socket.broadcast.emit('userdisconnected', {
+        id: socket.id,
+        name: user_name
+    })
+}
+
+exports.sendmessage = (socket,data) => {
+	socket.to(data.id).emit('newmessage', {
+        message: data.message,
+        name: "KUNAL" // get sender name from the UI refer /public/js/chatscript
+    })
+}
+
