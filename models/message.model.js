4fc25ffddd9264832fc18b2fe238905d82ff0940
kunal768 <33108756+kunal768@users.noreply.github.com>
2019-02-27 22:15:29 +0530

    Update message.model.js

--- models/message.model.js
+++ models/message.model.js
@@ -4,7 +4,7 @@ const User = require("../models/user.model")
 var MessageSchema = new Schema({
 	sender :  User.schema,
 	receiver :  User.schema,
-	timestamp : new Date().toLocaleTimeString('en-US',{hour12:true})
+	timestamp : new Date().toLocaleTimeString('en-US',{hour12:true}),
 	ViaEb : Boolean,
 	content : String,
 
