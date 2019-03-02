e5630b09ce44bd09a1e1562a81f13f259903e97b
Shubham Sharma <shubhamsharma1172@gmail.com>
2019-03-02 20:18:54 +0530

    logger changes

--- utils/logger.js
+++ utils/logger.js
@@ -1,18 +1,41 @@
-const winston = require('winston')
+const { createLogger, format, transports } = require('winston')
+const { combine, colorize, printf } = format
+const moment = require('moment-timezone')
 const config = require('../config')
 
-const transports = [
-  new winston.transports.File(
+const log_format = printf(info => {
+  return `(${info.timestamp}) [${info.level}] ${info.message}`
+})
+
+const timestamp = format((info) => {
+  info.timestamp = moment().tz('Asia/Kolkata').format('MMM Do YYYY h:m:s A z')
+  return info
+})
+
+const transports_list = [
+  new transports.File(
     {
       level: 'error',
       filename: config.error_log_file
+    }),
+  new transports.File(
+    {
+      level: 'debug',
+      filename: config.debug_log_file
     })
 ]
 
-if (process.env.NODE_ENV !== 'test'){
-  transports.push(new winston.transports.Console({level: 'info'}))
+if (process.env.NODE_ENV !== 'test') {
+  transports_list.push(new transports.Console({ level: 'info' }))
 }
 
-const logger = winston.createLogger({transports})
+const logger = createLogger({
+  format: combine(
+    colorize(),
+    timestamp(),
+    log_format
+  ),
+  transports: transports_list
+})
 
 module.exports = logger
