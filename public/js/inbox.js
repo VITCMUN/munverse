177e00c81464c113115c94cea026508b80f83c28
Amey Parundekar <parundekaramey@gmail.com>
2019-03-20 03:05:52 +0530

    Revamped UI/UX

--- public/js/inbox.js
+++ public/js/inbox.js
@@ -1,9 +1,20 @@
 $(document).ready(() => {
   $("#message_box").hide()
   $("#send_message").hide()
+  $("#via-eb-col").hide()
   $("#message_box").keyup(function(event) {
     if (event.keyCode === 13 && !event.shiftKey) {
         $("#send_message").click();
     }
   })
+
+  $('#via-eb-input').change(function() {
+    console.log("Pressed")
+      if(this.checked) {
+        $("#send-message-button").html("REPLY VIA EB");
+      }else{
+        $("#send-message-button").html("REPLY");
+      }
+  });
+
 });
